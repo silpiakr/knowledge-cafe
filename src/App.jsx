3b1b79031c66d8df2f7b371dd73b0950b09e5060
silpiAkter <silpiakter461@gmail.com>
2024-10-22 00:10:51 +0600

    added component file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      
+      
+    </>
+  )
+}
+
+export default App
